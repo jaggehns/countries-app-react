e61208a168f9eaa21159749be27357cb38046645
Jaggehn <jaggehns@gmail.com>
2023-04-09 20:20:36 +0800

    Setup Tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{js,jsx,ts,tsx}', '*.{html,js}'],
+  // darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
